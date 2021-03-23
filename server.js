867236013e9ac302a75d7d0408fa10b20c7bd079
Kristiyan Raykov <craykov@yahoo.com>
2021-03-23 19:24:22 +0200

    initialized handlebars

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
 
 app.get('/', (req, res) => {
     res.send('Hello express!')
