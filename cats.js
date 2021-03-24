0e8b38974d4a7a06a205db66060fade5ecc5f4e6
Kristiyan Raykov <craykov@yahoo.com>
2021-03-24 18:35:07 +0200

    added cats render

---
+++ cats.js
@@ -0,0 +1,15 @@
+const cats = [];
+
+function add(name) {
+    cats.push(name)
+
+
+}
+
+function getAll(){
+    return cats.slice();
+}
+module.exports = {
+    add,
+    getAll
+}
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
 
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
 
+const cats = require('./cats')
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', (req, res) => {
-    res.send('Hello express!')
+    let name = 'Something';
+    res.render('home', { name })
 })
 
+app.get('/cats', (req, res) => {
+    res.render('cats', {cats: cats.getAll()})
+})
+
+app.post('/cats', (req, res) => {
+    let catName = req.body.cat;
+    cats.add(catName)
+
+    res.redirect('/cats')
+})
 app.listen(5000, () => {
     console.log('this server is running on port 5000');
 })
